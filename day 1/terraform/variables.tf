5bd66efb9f1d41efe9e310a91a0efd392c21aa93
Mikhael Fourie <mikhael.fourie@gmail.com>
2026-05-05 11:43:56 +0200

    Update variables in Terraform configuration

--- day 1/terraform/variables.tf
+++ day 1/terraform/variables.tf
@@ -1,9 +1,44 @@
 variable "location" {
-  description = "Azure region for resources"
-  default     = "East US"
+  description = "Azure region for all resources"
+
+  type    = string
+  default = "eastus"
 }
 
-variable "env" {
-  description = "Environment name"
-  default     = "dev"
+variable "default_tags" {
+  type = map(string)
+  default = {
+    "Environment" = "dev"
+    "Source"      = "Terraform"
+    "Owner"       = "Mike"
+  }
+}
+
+variable "project_name" {
+  description = "Base name used for all resources"
+  type        = string
+  default     = "az-atmpt"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "subnet_1_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "az-atmpt-subnet-1"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+  sensitive   = true
 }
