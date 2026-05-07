64cc374cb949864512f681c2eb994c34b3f8f508
Mikhael Fourie <mikhael.fourie@gmail.com>
2026-05-07 21:16:12 +0200

    Define Azure resources and modules in main.tf
    Added resources for Azure infrastructure including resource group, storage account, public IP, and network interface. Configured modules for policy and network settings.

--- day 2/terraform/main.tf
+++ day 2/terraform/main.tf
@@ -1,1 +1,56 @@
-# soon to be added
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+  tags     = local.default_tags
+}
+
+module "policy" {
+  source = "../modules/policy"
+
+  scope = azurerm_resource_group.rg.id
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.default_tags
+}
+
+module "network" {
+  source = "../modules/network"
+
+  virtual_network     = var.virtual_network
+  subnet              = var.subnet
+  location            = var.location
+  resource_group_name = var.resource_group
+  address_space       = var.address_space
+  subnet_prefixes     = var.subnet_prefixes
+
+  default_tags = local.default_tags
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.public_ip
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  tags                = local.default_tags
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.network_interface_card
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.default_tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = module.network.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
